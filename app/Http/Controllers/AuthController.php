ed3f7c5085d1f9c44bd2d3bf6d10b6fb427e6a40
EngrSagorHossain <sagorhossain4217@gmail.com>
2023-11-30 12:12:34 +0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $token = $user->createToken('MyApp')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('MyApp')->plainTextToken;
+            return response()->json(['user' => $user, 'token' => $token], 200);
+        }
+
+        return response()->json(['error' => 'Tui paris nai'], 401);
+    }
+}
